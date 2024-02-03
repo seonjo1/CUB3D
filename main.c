0c16294ea683cd93536ac08a64a10d1925c5ef2b
michang <minseok128128@gmail.com>
2024-02-03 19:44:44 +0900

    Update main : mlx test

--- main.c
+++ main.c
@@ -10,7 +10,32 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+# include "minilibx/mlx.h"
+
+# define WIN_WIDTH 1920
+# define WIN_HEIGHT 1080
+
+typedef struct s_data {
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*addr;
+	int		endian;
+}	t_data;
+
 int	main(void)
 {
-	return (0);
+	t_data	data;
+
+	printf("here");
+	printf("here");
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, WIN_WIDTH, WIN_HEIGHT, "cub3d");
+	// data.img = mlx_new_image(data.mlx, WIN_WIDTH, WIN_HEIGHT);
+	// data.addr = mlx_get_data_addr(data.img, &(data.map.bpp),
+	// 		&(data.map.line_length), &(data.endian));
+	// mlx_hook(data.mlx_win, 2, 0, &keypress_event, &data);
+	// mlx_hook(data.mlx_win, 17, 0, &leave_event, &data);
+	mlx_loop(data.mlx);
 }
