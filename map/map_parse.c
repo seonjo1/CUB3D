ed9446505b48911430ac21e3c92f13ccec130839
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 17:33:17 +0900

    Update map_parse : map_check_arg 함수와 map_error 함수 추가

--- map/map_parse.c
+++ map/map_parse.c
@@ -6,28 +6,29 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:01 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 17:16:24 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/04 17:32:04 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
+void	map_error(char *msg)
+{
+	printf("error\n%s\n", msg);
+	exit(1);
+}
+
 void	map_check_arg(int argc, char **argv)
 {
-	int len;
-	
+	int	len;
+
 	if (argc < 1)
-	{
-		printf("erron\nToo few arguments\n");
-		exit(1);
-	}
+		map_error("too few arguments");
 	else if (argc > 1)
-	{
-		printf("erron\nToo many arguments\n");
-		exit(1);
-	}
+		map_error("too many arguments");
 	len = ft_strlen(argv[1]);
-	if (len )
+	if (len < 4 || ft_strncmp(argv[1] + (len - 4), ".cub", 5) != 0)
+		map_error("invalid extension");
 }
 
 int	*map_parse(int argc, char **argv)
