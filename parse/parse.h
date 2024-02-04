f238128d8fc136f276375770a2c77d5f1e724f65
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 17:48:01 +0900

    Update parse : 함수 이름들 parse로 변경

---
+++ parse/parse.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
+/*   Updated: 2024/02/04 17:42:07 by seonjo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_H
+# define PARSE_H
+
+# include "../cub3d.h"
+# include "../gnl/get_next_line.h"
+# include "../libft/libft.h"
+
+int	*parse_map(int argc, char **argv);
+
+#endif
