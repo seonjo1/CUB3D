ec68b95b7745172133796687a30fe4e43a77d33d
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 21:13:05 +0900

    Update Project : parse.h에 libft_s.h 추가 및 s_map 구조체에 data를 char *로 변경

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 20:17:44 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/04 21:06:14 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 # include <stdio.h>
 # include <fcntl.h>
 # include "../gnl/get_next_line.h"
-# include "../libft/libft.h"
+# include "../libft_s/libft_s.h"
 
 typedef struct s_map {
-	int	*data;
-	int	row;
-	int	col;
+	char	*data;
+	int		row;
+	int		col;
 }	t_map;
 
 void	parse_map(t_map *map, int argc, char **argv);
