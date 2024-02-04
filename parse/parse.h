0458785b0da913e78967987c06075e9b756f27fd
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 19:36:44 +0900

    Update Project : parse.h에 t_map 구조체 추가

--- parse/parse.h
+++ parse/parse.h
@@ -6,17 +6,23 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 17:42:07 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/04 18:10:18 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSE_H
 # define PARSE_H
 
-# include "../cub3d.h"
+# include <fcntl.h>
 # include "../gnl/get_next_line.h"
 # include "../libft/libft.h"
 
-int	*parse_map(int argc, char **argv);
+typedef struct s_map {
+	int	*data;
+	int	row;
+	int	col;
+}	t_map;
+
+void	parse_map(t_map *map, int argc, char **argv);
 
 #endif
