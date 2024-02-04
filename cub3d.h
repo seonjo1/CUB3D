4ba745a05f8d64aad78144d84875fee824d399b9
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 17:17:24 +0900

    Update Project : cub3d.h에 map.h추가

--- cub3d.h
+++ cub3d.h
@@ -6,13 +6,14 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:32:31 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 16:36:55 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/04 16:47:17 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 # include <stdio.h>
+# include "map/map.h"
 # include "minilibx/mlx.h"
 
 # define WIN_WIDTH 1920
