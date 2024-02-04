0de6469357a8c0a3cf6936a2a19ccae0b6e1a5f4
seonjo1 <tjdgus9684@gmail.com>
2024-02-04 17:43:19 +0900

    Update Project : map파일을 parse로 교체

--- cub3d.h
+++ cub3d.h
@@ -6,14 +6,14 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:32:31 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 16:47:17 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/04 17:41:46 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 # include <stdio.h>
-# include "map/map.h"
+# include "parse/parse.h"
 # include "minilibx/mlx.h"
 
 # define WIN_WIDTH 1920
