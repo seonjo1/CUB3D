f34cb4d265ea479df4656b4d2824e751ce7d61ef
seonjo1 <tjdgus9684@gmail.com>
2024-02-05 14:05:35 +0900

    Update parse : map에 starting point가 1개인지 check하는 parse_check_start_point 함수와 벽으로 map이 다 막혀있는지 확인하는 함수 parse_check_wall 추가

--- parse/parse.c
+++ parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:01 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 21:10:12 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/05 14:04:27 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -150,23 +150,94 @@ void	parse_make_map(t_map *map, char *file)
 	parse_close(fd);
 }
 
-// void	print_map(t_map *map)
-// {
-// 	for (int i = 0; i < map->row; i++)
-// 	{
-// 		for(int j = 0; j < map->col; j++)
-// 			printf("%c ", map->data[map->col * i + j]);
-// 		printf("\n");
-// 	}	
-// }
+int	parse_is_start_point(char c)
+{
+	if (c == 'W' || c == 'N' || c == 'S' || c == 'E')
+		return (1);
+	return (0);
+}
+
+void	parse_check_start_point(t_map *map)
+{
+	int	i;
+	int	j;
+	int	flag;
+
+	flag = 0;
+	i = 0;
+	while (i < map->row)
+	{
+		j = 0;
+		while (j < map->col)
+		{
+			if (parse_is_start_point(map->data[map->col * i + j]))
+			{
+				if (flag)
+					parse_error("invalid map");
+				else
+					flag = 1;
+			}
+			j++;
+		}
+		i++;
+	}
+	if (!flag)
+		parse_error("invalid map");
+}
+
+void parse_dfs(t_map *map, char *check, int i, int j)
+{
+	if (check[map->col * i + j])
+		return ;
+	check[map->col * i + j] = 1;
+	if (map->data[map->col * i + j] == ' ')
+		parse_error("invalid map");
+	if (map->data[map->col * i + j] == '1')
+		return ;
+	if (i == 0 || i == map->row || j == 0 || j == map->col)
+		parse_error("invalid map");
+	parse_dfs(map, check, i - 1, j);
+	parse_dfs(map, check, i + 1, j);
+	parse_dfs(map, check, i, j - 1);
+	parse_dfs(map, check, i, j + 1);
+}
+
+void	parse_check_wall(t_map *map)
+{
+	int		i;
+	int		j;
+	int		flag;
+	char	*check;
+
+	check = ft_calloc_s(map->row * map->col, sizeof(char));
+	flag = 0;
+	i = 0;
+	while (i < map->row)
+	{
+		j = 0;
+		while (j < map->col)
+		{
+			if (map->data[map->col * i + j] == '0' || \
+				parse_is_start_point(map->data[map->col * i + j]))
+				parse_dfs(map, check, i, j);
+			j++;
+		}
+		i++;
+	}
+	free(check);
+}
+
+void	parse_check_map(t_map *map)
+{
+	parse_check_start_point(map);
+	parse_check_wall(map);
+}
 
 void	parse_map(t_map *map, int argc, char **argv)
 {	
 	ft_memset(map, 0, sizeof(t_map));
 	parse_check_arg(argc, argv);
 	parse_get_map_size(map, argv[1]);
-	printf("row : %d, col : %d\n", map->row, map->col);
 	parse_make_map(map, argv[1]);
-	// print_map(map);
-	// parse_check_map(map);
+	parse_check_map(map);
 }
