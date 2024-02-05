db422ebf5a1929d4a2e352244fb956a9d3c449f1
michang <minseok128128@gmail.com>
2024-02-05 22:19:52 +0900

    Update Project : player 구조체도 data에 포함

--- cub3d.h
+++ cub3d.h
@@ -14,12 +14,29 @@
 # define CUB3D_H
 # include <stdio.h>
 # include <fcntl.h>
+# include <math.h>
 # include "gnl/get_next_line.h"
 # include "libft_s/libft_s.h"
 # include "minilibx/mlx.h"
 
 # define WIN_WIDTH 1920
 # define WIN_HEIGHT 1080
+
+typedef struct s_vec2 {
+	double	x;
+	double	y;
+}	t_vec2;
+
+typedef struct s_intvec2 {
+	int	x;
+	int	y;
+}	t_intvec2;
+
+typedef struct s_player {
+	t_vec2	pos;
+	t_vec2	dir;
+	t_vec2	plane;
+}	t_player;
 
 typedef struct s_map {
 	char	**data;
@@ -28,25 +45,16 @@ typedef struct s_map {
 }	t_map;
 
 typedef struct s_data {
-	void	*mlx;
-	void	*mlx_win;
-	void	*img;
-	char	*addr;
-	int		bpp;
-	int		line_length;
-	int		endian;
-	t_map	map;
+	void		*mlx;
+	void		*mlx_win;
+	void		*img;
+	char		*addr;
+	int			bpp;
+	int			line_length;
+	int			endian;
+	t_map		map;
+	t_player	player;
 }	t_data;
 
-typedef struct s_vec2 {
-	double	x;
-	double	y;
-}	t_vec2;
-
-typedef struct s_player {
-	t_vec2	pos;
-	t_vec2	dir;
-	t_vec2	plane;
-}	t_player;
 
 #endif
