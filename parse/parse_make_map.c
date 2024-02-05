88a3ae52ec1c30155bc23e85db23ea2595f1c315
seonjo1 <tjdgus9684@gmail.com>
2024-02-05 14:13:11 +0900

    Add parse_make_map : parse.c에서 map을 만드는 함수 분리

---
+++ parse/parse_make_map.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_make_map.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 14:12:00 by seonjo            #+#    #+#             */
+/*   Updated: 2024/02/05 14:12:22 by seonjo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parse.c"
+
+int	parse_fill_map(t_map *map, char *line, int *col)
+{
+	int	i;
+	int	is_valid_line;
+
+	if (line == NULL)
+		return (0);
+	i = 0;
+	is_valid_line = 0;
+	while (line[i] != '\0' && line[i] != '\n')
+	{
+		if (line[i] != ' ')
+			is_valid_line = 1;
+		map->data[*col * map->col + i] = line[i];
+		i++;
+	}
+	while (i < map->col)
+		map->data[*col * map->col + i++] = ' ';
+	free(line);
+	if (!is_valid_line && (*col))
+		return (0);
+	else if (is_valid_line)
+		(*col)++;
+	return (1);
+}
+
+void	parse_make_map(t_map *map, char *file)
+{
+	int	fd;
+	int	col;
+
+	map->data = ft_calloc_s(map->row * map->col, sizeof(int));
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		parse_error("unable to open file");
+	col = 0;
+	while (parse_fill_map(map, gnl(fd), &col))
+		;
+	parse_close(fd);
+}
