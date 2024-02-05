f2b3c48e0badc8c7961faa5924a0c2a5927f4f07
seonjo1 <tjdgus9684@gmail.com>
2024-02-05 14:46:35 +0900

    Update Project : parse.c에 있던 함수 분리

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 21:06:14 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/05 14:12:30 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,10 @@ typedef struct s_map {
 }	t_map;
 
 void	parse_map(t_map *map, int argc, char **argv);
+void	parse_check_map(t_map *map);
+void	parse_sizing_map(t_map *map, char *file);
+void	parse_error(char *msg);
+int		parse_close(int fd);
+void	parse_make_map(t_map *map, char *file);
 
 #endif
