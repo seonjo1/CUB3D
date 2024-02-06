f51667c5f037df9fb07ff432d7c8d7c205a38181
michang <minseok128128@gmail.com>
2024-02-06 16:37:40 +0900

    Update Project : libft_s 헤더에 vec 함수 명세

--- libft_s/libft_s.h
+++ libft_s/libft_s.h
@@ -12,9 +12,11 @@
 
 #ifndef LIBFT_S_H
 # define LIBFT_S_H
-
-# include "../libft/libft.h"
+# include "../cub3d.h"
 
 void	*ft_calloc_s(size_t count, size_t size);
+t_vec2	vec2_creat(double x, double y);
+t_vec2	vec2_add(t_vec2 a, t_vec2 b);
+t_vec2	vec2_scala_mul(t_vec2 a, double b);
 
 #endif
