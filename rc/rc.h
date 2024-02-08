12c5383d5874525480c1c51525c73c1303925094
seonjo1 <tjdgus9684@gmail.com>
2024-02-08 15:32:46 +0900

    Update Project : rc.h 개행 제거

--- rc/rc.h
+++ rc/rc.h
@@ -6,13 +6,12 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 13:59:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/08 15:24:12 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/08 15:30:56 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RC_H
 # define RC_H
-
 # include "../cub3d.h"
 
 double	rc_get_distance(t_data *data, t_rc_data *rc_data, t_vec2 ray);
