4cc28e3cedaab5e2606f54910cfd0b0eedba80dd
seonjo1 <tjdgus9684@gmail.com>
2024-02-08 20:19:38 +0900

    Update libft_s : ft_split_s 추가

--- libft_s/libft_s.c
+++ libft_s/libft_s.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 20:55:27 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/04 20:57:00 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/08 20:19:25 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,16 @@ void	*ft_calloc_s(size_t count, size_t size)
 	void	*new;
 
 	new = ft_calloc(count, size);
+	if (!new)
+		exit(1);
+	return (new);
+}
+
+char	**ft_split_s(const char *str, char c)
+{
+	void	**new;
+
+	new = ft_split(str, c);
 	if (!new)
 		exit(1);
 	return (new);
