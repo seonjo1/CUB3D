8940622f7af190e4652c0f8d5392760439861cc7
seonjo1 <tjdgus9684@gmail.com>
2024-02-08 21:30:38 +0900

    Update parse_util : parse_atoi 함수 추가

--- parse/parse_utils.c
+++ parse/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 14:11:05 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/05 14:11:29 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/08 21:28:51 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,50 @@ int	parse_close(int fd)
 {
 	close(fd);
 	return (1);
+}
+
+char	*parse_sign_check(char *str, long long *minus)
+{
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			*minus = *minus * -1;
+		str++;
+	}
+	return (str);
+}
+
+long long	parse_overflow_check(long long num, long long i)
+{
+	if (i > 10)
+		return (-1);
+	if (num < 0 || num > 2147483647)
+		return (-1);
+	return (num);
+}
+
+long long	parse_atoi(char *str)
+{
+	long long	minus;
+	long long	i;
+	long long	num;
+
+	i = 0;
+	num = 0;
+	minus = 1;
+	while (*str >= 9 && *str <= 13)
+		str++;
+	str = parse_sign_check(str, &minus);
+	if (!(*str >= '0' && *str <= '9'))
+		return (-1);
+	while (*str >= '0' && *str <= '9')
+	{
+		num = num * 10 + *str - '0';
+		str++;
+		if (num > 0)
+			i++;
+	}
+	if (*str != '\0')
+		return (-1);
+	return (parse_overflow_check(num * minus, i));
 }
