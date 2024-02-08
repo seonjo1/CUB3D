ce180186276780eb5cb65a78c861bb7735b92779
seonjo1 <tjdgus9684@gmail.com>
2024-02-08 15:30:00 +0900

    Update Project : rc.h에 rc_get_distance, rc_raycast 함수 추가

--- rc/rc.h
+++ rc/rc.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 13:59:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/08 14:02:36 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/08 15:24:12 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 # define RC_H
 
 # include "../cub3d.h"
+
+double	rc_get_distance(t_data *data, t_rc_data *rc_data, t_vec2 ray);
+void	rc_raycast(t_data *data);
 
 #endif
