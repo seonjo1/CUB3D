69a117cd58e1b75cdd1203a7897b437cc180552f
seonjo1 <tjdgus9684@gmail.com>
2024-02-10 17:03:35 +0900

    Update Project : parse.h에 parse_is_valid_lien 함수 추가

--- parse/parse.h
+++ parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/10 13:45:54 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/10 16:23:04 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int			parse_close(int fd);
 void		parse_make_map(t_data *data, char *file);
 void		parse_texture(t_data *data, int fd);
 long long	parse_atoi(char *str);
+int			parse_is_valid_line(char *line, int fd);
 
 #endif
