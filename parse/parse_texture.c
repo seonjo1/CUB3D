1226722c354ce3fe569dfefb9e392c02616723bb
seonjo1 <tjdgus9684@gmail.com>
2024-02-10 13:35:06 +0900

    Update parse_texture : parse_open_texture 함수를 parse_tex_file로 이름 변경

--- parse/parse_texture.c
+++ parse/parse_texture.c
@@ -6,13 +6,13 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:57:08 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/10 13:25:23 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/10 13:34:45 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int	parse_open_texture(t_tex *tex, char *arr, int type, int *element)
+int	parse_tex_file(t_tex *tex, char *arr, int type, int *element)
 {
 	if (*element & type)
 		return (1);
@@ -96,13 +96,13 @@ void	parse_identifier_check(t_data *data, char **arr, int fd, int *element)
 	else if (!arr[1])
 		flag = 1;
 	else if (ft_strncmp(arr[0], "EA", 3) == 0)
-		flag = parse_open_texture(&(data->tex[0]), arr[1], TX_EA, *element);
+		flag = parse_tex_file(&(data->tex[0]), arr[1], TX_EA, *element);
 	else if (ft_strncmp(arr[0], "WE", 3) == 0)
-		flag = parse_open_texture(&(data->tex[1]), arr[1], TX_WE, *element);
+		flag = parse_tex_file(&(data->tex[1]), arr[1], TX_WE, *element);
 	else if (ft_strncmp(arr[0], "SO", 3) == 0)
-		flag = parse_open_texture(&(data->tex[2]), arr[1], TX_SO, *element);
+		flag = parse_tex_file(&(data->tex[2]), arr[1], TX_SO, *element);
 	else if (ft_strncmp(arr[0], "NO", 3) == 0)
-		flag = parse_open_texture(&(data->tex[3]), arr[1], TX_NO, *element);
+		flag = parse_tex_file(&(data->tex[3]), arr[1], TX_NO, *element);
 	else if (ft_strncmp(arr[0], "C", 2) == 0)
 		flag = parse_tex_color(&(data->c_color), arr[1], TX_C, *element);
 	else if (ft_strncmp(arr[0], "F", 2) == 0)
