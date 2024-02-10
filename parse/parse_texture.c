5dbf896bff7ffc587335333a968fde6d3346de5c
seonjo1 <tjdgus9684@gmail.com>
2024-02-10 19:14:43 +0900

    Update parse_texture : color 파싱부분 분리

--- parse/parse_texture.c
+++ parse/parse_texture.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:57:08 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/10 18:13:42 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/10 19:08:13 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,73 +20,6 @@ int	parse_tex_file(t_tex *tex, char *str, int type, int *element)
 	*element |= type;
 	tex->file = ft_strdup_s(str);
 	tex->file[ft_strlen(tex->file) - 1] = '\0';
-	return (0);
-}
-
-int	parse_comma_check(char *str)
-{
-	int	i;
-	int	comma_num;
-
-	i = 0;
-	comma_num = 0;
-	while (str[i])
-	{
-		if (str[i++] == ',')
-			comma_num++;
-	}
-	if (comma_num == 2)
-		return (0);
-	else
-		return (1);
-}
-
-int	parse_color_check(char *str)
-{
-	char	**arr;
-	int		i;
-	int		num;
-	int		color;
-	int		flag;
-
-	arr = ft_split_s(str, ',');
-	i = 0;
-	flag = 0;
-	color = 0;
-	while (arr[i])
-	{
-		num = parse_atoi(arr[i]);
-		if (num == -1)
-		{
-			flag = 1;
-			break ;
-		}
-		color = (color << 8) | num;
-		i++;
-	}
-	i = 0;
-	while (arr[i])
-		free(arr[i++]);
-	free(arr);
-	if (flag || i != 3)
-		return (-1);
-	else
-		return (color);
-}
-
-int	parse_tex_color(int *tex_color, char *str, int type, int *element)
-{
-	int	input_color;
-
-	if (*element & type)
-		return (1);
-	*element |= type;
-	if (parse_comma_check(str))
-		return (1);
-	input_color = parse_color_check(str);
-	if (input_color == -1)
-		return (1);
-	*tex_color = input_color;
 	return (0);
 }
 
