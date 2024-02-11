b356a992f141e4403587ff60478fbab526e81e3b
michang <minseok128128@gmail.com>
2024-02-11 17:20:59 +0900

    Update Project : play_action_flash 함수 선언

--- play/play.h
+++ play/play.h
@@ -18,5 +18,8 @@ void	play_update(t_data *data);
 void	play_action_movement(t_player *player);
 void	play_action_jump(t_player *player, char *transition, char enter);
 void	play_action_crouch(t_player *player, char *transition, char enter);
+void	play_action_flash(t_player *player, char *transition, char enter);
 int		play_target_update(double *target, double goal, double delta, double offset);
+
+
 #endif
