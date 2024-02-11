4f0e40a3d927d985894492d4394268ba517f987a
michang <minseok128128@gmail.com>
2024-02-11 21:52:24 +0900

    Update play_recall : recall 배열 꾸준한 저장 및 출력 확인

--- play/play_recall.c
+++ play/play_recall.c
@@ -12,7 +12,47 @@
 
 #include "play.h"
 
-void	play_action_recall()
+void	play_test_lst_print(t_player *player)
 {
-	
+	t_vec3	*pos;
+	t_vec2	*euler_dir;
+	int		idx;
+
+	pos = player->recall.pos;
+	euler_dir = player->recall.euler_dir;
+	idx = player->recall.idx;
+	for (int i = 0; i < 10; i++)
+	{
+		if (i != 0)
+			printf(" ==> ");
+		printf("(id:%d, x:%.3f, y:%.3f", (idx + i) % 10, pos[(idx + i) % 10].x, pos[(idx + i) % 10].y);
+		printf(", yaw: %.3f)", euler_dir[(idx + i) % 10].y);
+	}
+	printf("\n\n");
+}
+
+void	play_action_recall(t_player *player, t_recall *re, char enter)
+{
+	static int	save_freq = 0;
+
+	if (enter == ENTER)
+	{
+		player->keybinds = (player->keybinds & ~(1 << KB_RECALL));
+		ft_strlcpy(player->state, "__R", 4);
+	}
+	else if (enter == RUN)
+	{
+		if (player->state[2] == 'R')
+		{
+
+		}
+		else if (++save_freq > 60)
+		{
+			re->idx++;
+			re->pos[re->idx] = player->pos;
+			re->euler_dir[re->idx] = player->euler_dir;
+			save_freq = 0;
+			play_test_lst_print(player);
+		}
+	}
 }
