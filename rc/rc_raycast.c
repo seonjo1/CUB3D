9f48ca3f2660ddd11da89f0714f081e042ca88e7
seonjo1 <tjdgus9684@gmail.com>
2024-02-12 21:48:20 +0900

    Update rc_raycast : 텍스처 구현을 했지만 가까이가면 텍스처가 줄어드는 현상 발생

--- rc/rc_raycast.c
+++ rc/rc_raycast.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 13:58:46 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/08 15:25:37 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/12 21:45:01 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,30 @@ void	rc_draw_col(t_data *data, t_vec2 ray, int x)
 	int			i;
 	int			color;
 	double		dis;
+	int			tex_x;
 	t_height	h;
 	t_rc_data	rc_data;
 
 	dis = rc_get_distance(data, &rc_data, ray);
+
+	double	wall_x;
+	int		type;
+	
+	if (rc_data.side && ray.x > 0)
+		type = TC_EA;
+	else if (rc_data.side && ray.x < 0)
+		type = TC_WE;
+	else if (ray.y > 0)
+		type = TC_SO;
+	else
+		type = TC_NO;
+	if (rc_data.side)
+		wall_x = data->player.pos.y + dis * ray.y;
+	else
+		wall_x = data->player.pos.x + dis * ray.x;
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * (double)data->tex[type].width);
+	
 	h.height = (int)(WIN_HEIGHT / dis) * FOV_BASE / data->player.fov;
 	h.draw_start = -h.height / 2 + WIN_HEIGHT / 2 + data->player.pos.z / dis;
 	if (h.draw_start < 0)
@@ -29,15 +49,18 @@ void	rc_draw_col(t_data *data, t_vec2 ray, int x)
 	if (h.draw_end >= WIN_HEIGHT)
 		h.draw_end = WIN_HEIGHT - 1;
 	i = 0;
-	color = 0x615f5f;
 	if (rc_data.side)
 		color = 0x777777;
 	while (i < h.draw_start)
-		utils_draw_point(data, x, i++, 0x555555);
+		utils_draw_point(data, x, i++, data->c_color);
+	int tex_height = h.draw_end - h.draw_start;
 	while (i < h.draw_end)
+	{
+		color = data->tex[type].data[((i - h.draw_start) * data->tex[type].height) / tex_height][tex_x];
 		utils_draw_point(data, x, i++, color);
+	}
 	while (i < WIN_HEIGHT)
-		utils_draw_point(data, x, i++, 0x494a4e);
+		utils_draw_point(data, x, i++, data->f_color);
 }
 
 void	rc_raycast(t_data *data)
