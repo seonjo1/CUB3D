30ec15bf1f7606beeb5d90afedb86fc6e2860416
michang <minseok128128@gmail.com>
2024-02-14 18:57:19 +0900

    Udpate hand : hand_init_each 구현

--- hand/hand.c
+++ hand/hand.c
@@ -11,25 +11,32 @@
 /* ************************************************************************** */
 
 #include "hand.h"
+#include "../libft_s/libft_s.h"
 
-void	hand_init_each(void **arr, int max, char *name)
+void	hand_init_each(t_data *data, void **arr, int max, char *base)
 {
-	int	i;
+	char	*tmp;
+	char	*file_name;
+	int		i;
+	int		w;
+	int		h;
 
 	i = 0;
 	while (i < max)
 	{
-		
+		tmp = ft_itoa_s(i);
+		file_name = ft_strjoin_s(base, tmp);
+		free(tmp);
+		tmp = file_name;
+		file_name = ft_strjoin_s(file_name, ".xpm");
+		free(tmp);
+		arr[i] = mlx_xpm_file_to_image(data->mlx, file_name, &w, &h);
+		free(file_name);
+		i++;
 	}
 }
 
 void	hand_init_xpm_imgs(t_data *data)
 {
-	int	i;
-
-	i = -1;
-	while ()
-	{
-
-	}
+	hand_init_each(data, &(data->hand_res.flash), HN_FLASH, "../res/flash");
 }
