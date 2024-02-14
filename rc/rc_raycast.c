fec078a25e80c202fbfd36c21058ad54ea54ed75
seonjo1 <tjdgus9684@gmail.com>
2024-02-14 16:51:16 +0900

    Update rc_raycast : refectoring

--- rc/rc_raycast.c
+++ rc/rc_raycast.c
@@ -6,60 +6,56 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 13:58:46 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/14 16:29:39 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/14 16:48:00 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rc.h"
 
+void	rc_get_tex_x(t_data *data, t_rc_data *rc_data, t_draw_data *d_data, t_vec2 ray)
+{
+	if (rc_data->side && ray.x > 0)
+		d_data->type = TC_EA;
+	else if (rc_data->side && ray.x < 0)
+		d_data->type = TC_WE;
+	else if (ray.y > 0)
+		d_data->type = TC_SO;
+	else
+		d_data->type = TC_NO;
+	if (rc_data->side)
+		d_data->wall_x = data->player.pos.y + d_data->dist * ray.y;
+	else
+		d_data->wall_x = data->player.pos.x + d_data->dist * ray.x;
+	if (d_data->type == TC_NO || d_data->type == TC_EA)
+		d_data->wall_x = d_data->wall_x - floor(d_data->wall_x);
+	else
+		d_data->wall_x = ceil(d_data->wall_x) - d_data->wall_x;
+	d_data->tex_x =	d_data->wall_x * (double)data->tex[d_data->type].width;
+}
+
 void	rc_draw_col(t_data *data, t_vec2 ray, int x)
 {
 	int			i;
-	int			color;
-	double		dis;
-	int			tex_x;
-	t_height	h;
+	t_draw_data	d_data;
 	t_rc_data	rc_data;
 
-	dis = rc_get_distance(data, &rc_data, ray);
-
-	double	wall_x;
-	int		type;
-	
-	if (rc_data.side && ray.x > 0)
-		type = TC_EA;
-	else if (rc_data.side && ray.x < 0)
-		type = TC_WE;
-	else if (ray.y > 0)
-		type = TC_SO;
-	else
-		type = TC_NO;
-	if (rc_data.side)
-		wall_x = data->player.pos.y + dis * ray.y;
-	else
-		wall_x = data->player.pos.x + dis * ray.x;
-	if (type == TC_NO || type == TC_EA)
-		wall_x = wall_x - floor(wall_x);
-	else
-		wall_x = ceil(wall_x) - wall_x;
-	tex_x =	wall_x * (double)data->tex[type].width;
-	h.height = (int)(WIN_HEIGHT / dis) * FOV_BASE / data->player.fov;
-	h.draw_start = -h.height / 2 + WIN_HEIGHT / 2 + data->player.pos.z / dis;
-	h.draw_end = h.height / 2 + WIN_HEIGHT / 2 + data->player.pos.z / dis;
-	int start = h.draw_start;
-	int	end = h.draw_end;
-	if (h.draw_start < 0)
-		h.draw_start = 0;
-	if (h.draw_end >= WIN_HEIGHT)
-		h.draw_end = WIN_HEIGHT - 1;
+	rc_get_distance(data, &rc_data, &d_data, ray);
+	rc_get_tex_x(data, &rc_data, &d_data, ray);
+	d_data.tex_h = (int)(WIN_HEIGHT / d_data.dist) * FOV_BASE / data->player.fov;
+	d_data.start = -d_data.tex_h / 2 + WIN_HEIGHT / 2 + data->player.pos.z / d_data.dist;
+	d_data.end = d_data.tex_h / 2 + WIN_HEIGHT / 2 + data->player.pos.z / d_data.dist;
+	d_data.offset = d_data.start;
+	if (d_data.start < 0)
+		d_data.start = 0;
+	if (d_data.end >= WIN_HEIGHT)
+		d_data.end = WIN_HEIGHT - 1;
 	i = 0;
-	while (i < h.draw_start)
+	while (i < d_data.start)
 		utils_draw_point(data, x, i++, data->c_color);
-	int tex_height = end - start;
-	while (i < h.draw_end)
+	while (i < d_data.end)
 	{
-		color = data->tex[type].data[((i - start) * data->tex[type].height) / tex_height][(int)tex_x];
-		utils_draw_point(data, x, i++, color);
+		d_data.color = data->tex[d_data.type].data[((i - d_data.offset) * data->tex[d_data.type].height) / d_data.tex_h][d_data.tex_x];
+		utils_draw_point(data, x, i++, d_data.color);
 	}
 	while (i < WIN_HEIGHT)
 		utils_draw_point(data, x, i++, data->f_color);
