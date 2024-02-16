d01ee20b72df471a8a3d90dac4237f3e787cd502
michang <minseok128128@gmail.com>
2024-02-16 19:32:58 +0900

    Update hand : hand_action_attack 구현

--- hand/hand.c
+++ hand/hand.c
@@ -91,10 +91,22 @@ void	*hand_action_shot(void **arr)
 {
 	static int	t = 0;
 
-	if (++t == HN_SHOT)
+	if (++t == (HN_SHOT << 1))
 		t = 0;
-	printf("shot:%d\n", t);
-	return (arr[t]);
+	return (arr[t >> 1]);
+}
+
+void	*hand_action_attack(void **arr, int *kb)
+{
+	static int	t = 0;
+
+	if (++t == (HN_ATTACK << 1))
+	{
+		(*kb) = (*kb & ~(1 << KB_ATTACK));
+		t = 0;
+		return (arr[KB_ATTACK - 1]);
+	}
+	return (arr[(t - 1) >> 1]);
 }
 
 void	*hand_update(t_data *data)
@@ -109,6 +121,8 @@ void	*hand_update(t_data *data)
 		hand = hand_action_recall(data->h_res.recall, data->player.recall.frame);
 	else if (data->player.keybinds & (1 << KB_M_LEFT))
 		hand = hand_action_shot(data->h_res.shot);
+	else if (data->player.keybinds & (1 << KB_ATTACK))
+		hand = hand_action_attack(data->h_res.attack, &(data->player.keybinds));
 	else if (!ft_strncmp(ps, "W__", 4))
 		hand = hand_action_walk(&(data->player), data->h_res.walk, 3);
 	else if (!ft_strncmp(ps, "R__", 4))
