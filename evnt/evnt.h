829beda63f509fc330431d1af59ecb8d79655cbb
michang <minseok128128@gmail.com>
2024-02-16 19:33:18 +0900

    Update evnt.h : evnt_x_set 정의

--- evnt/evnt.h
+++ evnt/evnt.h
@@ -20,6 +20,6 @@ int		evnt_mousepress(int keycode, int x, int y, t_player *player);
 int		evnt_mouserelease(int keycode, int x, int y, t_player *player);
 int		evnt_leave(int keycode, int tmp);
 void	evnt_keybinds_set(int *kb, int keycode, long long time, char press);
-void	evnt_shift_set(int *kb, char press);
+void	evnt_x_set(int *kb, char press, int t_kb, int num);
 
 #endif
