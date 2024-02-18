bb14fa909807438cf1db45c219abf02e5b1abddf
seonjo1 <tjdgus9684@gmail.com>
2024-02-18 14:56:58 +0900

    Update rc_draw_floor : fov 변경 적용

--- rc/rc_draw_floor.c
+++ rc/rc_draw_floor.c
@@ -17,6 +17,7 @@ void rc_draw_floor(t_data *data)
 {
 	int		x;
 	int		y;
+	int		camera_y;
 	double	camera_x;
 	t_vec2	ray;
 
@@ -27,15 +28,16 @@ void rc_draw_floor(t_data *data)
 		while (x < WIN_WIDTH)
 		{
 			camera_x = 2 * x / (double) WIN_WIDTH - 1;
+			camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
 			ray.x = data->player.dir.x + data->player.plane.x * camera_x;
 			ray.y = data->player.dir.y + data->player.plane.y * camera_x;
-			double floor_x = data->player.pos.x + ray.x * (WIN_HEIGHT / 2) / y;
-			double floor_y = data->player.pos.y + ray.y * (WIN_HEIGHT / 2) / y;
+			double floor_x = data->player.pos.x + ray.x * (camera_y / 2) / y;
+			double floor_y = data->player.pos.y + ray.y * (camera_y / 2) / y;
 			if (floor_x >= 0 && floor_y >= 0 && floor_x < data->map.col && floor_y < data->map.row)
 			{
 				int tex_x = (int)((ceil(floor_x) - floor_x) * data->tex[TC_F].width);
-				int tex_y = (int)((ceil(floor_y) - floor_y) * data->tex[TC_F].width);
-				int color = data->tex[TC_F].data[tex_x][tex_y];
+				int tex_y = (int)((ceil(floor_y) - floor_y) * data->tex[TC_F].height);
+				int color = data->tex[TC_F].data[tex_y][tex_x];
 				utils_draw_point(data, x, y + WIN_HEIGHT / 2, color);
 			}
 			x++;
