dbb3c3dc70bd8cdbceaa6f42d30ccc3a99597811
seonjo1 <tjdgus9684@gmail.com>
2024-02-18 15:23:40 +0900

    Update parse_tex : floor와 ceiling 위치 변환

--- parse/parse_tex.c
+++ parse/parse_tex.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:57:08 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/16 17:49:54 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/18 15:21:45 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,14 @@ void	parse_identifier_check(t_data *data, char **arr, int fd, int *element)
 		flag = parse_tex_file(&(data->tex[2]), arr[1], PC_SO, element);
 	else if (ft_strncmp(arr[0], "NO", 3) == 0)
 		flag = parse_tex_file(&(data->tex[3]), arr[1], PC_NO, element);
-	else if (ft_strncmp(arr[0], "CT", 3) == 0)
-		flag = parse_tex_file(&(data->tex[4]), arr[1], PC_CT, element);
 	else if (ft_strncmp(arr[0], "FT", 3) == 0)
-		flag = parse_tex_file(&(data->tex[5]), arr[1], PC_FT, element);
-	else if (ft_strncmp(arr[0], "C", 2) == 0)
-		flag = parse_tex_color(&(data->c_color), arr[1], PC_C, element);
+		flag = parse_tex_file(&(data->tex[4]), arr[1], PC_FT, element);
+	else if (ft_strncmp(arr[0], "CT", 3) == 0)
+		flag = parse_tex_file(&(data->tex[5]), arr[1], PC_CT, element);
 	else if (ft_strncmp(arr[0], "F", 2) == 0)
-		flag = parse_tex_color(&(data->f_color), arr[1], PC_F, element);
+		flag = parse_tex_color(&(data->c_color), arr[1], PC_F, element);
+	else if (ft_strncmp(arr[0], "C", 2) == 0)
+		flag = parse_tex_color(&(data->f_color), arr[1], PC_C, element);
 	else
 		flag = 1;
 	if ((flag || arr[2] != NULL) && parse_close(fd))
