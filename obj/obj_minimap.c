1769aadea4d91befb23c9808b7b98f558dc52a4c
michang <minseok128128@gmail.com>
2024-02-23 15:02:51 +0900

    Update obj_minimap : 기존의 mini 구조체를 통해 그리는 것을 data에 그림

--- obj/obj_minimap.c
+++ obj/obj_minimap.c
@@ -20,11 +20,11 @@ void	obj_mini_init(t_data *data)
 		data->mini.size = WIN_WIDTH / 5;
 	if (!(data->mini.size % 2))
 		data->mini.size += 1;
-	data->mini.img = mlx_new_image(data->mlx, data->mini.size, data->mini.size);
-	if (!data->mini.img)
+	data->img = mlx_new_image(data->mlx, data->mini.size, data->mini.size);
+	if (!data->img)
 		exit(1);
-	data->mini.addr = mlx_get_data_addr(data->mini.img, &(data->mini.bpp), &(data->mini.line_length), &(data->mini.endian));
-	if (!data->mini.addr)
+	data->addr = mlx_get_data_addr(data->img, &(data->bpp), &(data->line_length), &(data->endian));
+	if (!data->addr)
 		exit(1);
 }
 
@@ -46,12 +46,12 @@ void	obj_draw_minimap(t_data *data, t_mini *mini)
 			x = (double)j / 6.0 + mini->start_j;
 			y = (double)i / 6.0 + mini->start_i;
 			if (y >= 0 && y < data->map.row && x >= 0 && x < data->map.col && data->map.data[(int)y][(int)x] == '1')
-				*(int *)(mini->addr + i * mini->line_length + j * (mini->bpp / 8)) = 0xCC0099;
+				*(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)) = 0xCC0099;
 			else
-				*(int *)(mini->addr + i * mini->line_length + j * (mini->bpp / 8)) = 0xCC000000;
+				*(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)) = 0xCC000000;
 			j++;
 		}
 		i++;
 	}
-	*(int *)(mini->addr + (mini->size / 2) * mini->line_length + (mini->size / 2) * (mini->bpp / 8)) = 0xffffff;
+	*(int *)(data->addr + (mini->size / 2) * data->line_length + (mini->size / 2) * (data->bpp / 8)) = 0xffffff;
 }
