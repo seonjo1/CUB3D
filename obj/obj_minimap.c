64c44c75cd02d31b01312f246effe661f546eb87
michang <minseok128128@gmail.com>
2024-02-23 16:15:35 +0900

    Update obj_minimap : aim 그리는 함수 추가 및, 반투명 효과 제대로 적용

--- obj/obj_minimap.c
+++ obj/obj_minimap.c
@@ -11,6 +11,25 @@
 /* ************************************************************************** */
 
 #include "obj.h"
+#include "../libft_s/libft_s.h"
+
+void	obj_draw_aim(t_data *data)
+{
+	t_intvec2	aim;
+	int			i;
+	const int	aim_size = 20;
+
+	aim.x = WIN_WIDTH / 2 - aim_size;
+	aim.y = WIN_HEIGHT / 2;
+	i = -1;
+	while (++i < aim_size * 2)
+		utils_draw_point(data, aim.x + i, aim.y, 0xFF00FF);
+	aim.x = WIN_WIDTH / 2;
+	aim.y = WIN_HEIGHT / 2 - aim_size;
+	i = -1;
+	while (++i < aim_size * 2)
+		utils_draw_point(data, aim.x, aim.y + i, 0xFF00FF);
+}
 
 void	obj_mini_init(t_data *data)
 {
@@ -24,13 +43,14 @@ void	obj_mini_init(t_data *data)
 
 void	obj_draw_minimap(t_data *data, t_mini *mini)
 {
-	int		i;
-	int		j;
-	double	x;
-	double	y;
+	int			i;
+	int			j;
+	t_intvec2	color;
+	double		x;
+	double		y;
 
-	mini->start_i = data->player.pos.y - (data->mini.size / 2) / 6;
-	mini->start_j = data->player.pos.x - (data->mini.size / 2) / 6;
+	mini->start_i = data->player.pos.y - (data->mini.size >> 1) / 6;
+	mini->start_j = data->player.pos.x - (data->mini.size >> 1) / 6;
 	i = 0;
 	while (i < mini->size)
 	{
@@ -39,10 +59,12 @@ void	obj_draw_minimap(t_data *data, t_mini *mini)
 		{
 			x = (double)j / 6.0 + mini->start_j;
 			y = (double)i / 6.0 + mini->start_i;
+			color.x = utils_blend_color(0xCC0099, *(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)), 0.5);
+			color.y = utils_blend_color(0x000000, *(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)), 0.5);
 			if (y >= 0 && y < data->map.row && x >= 0 && x < data->map.col && data->map.data[(int)y][(int)x] == '1')
-				*(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)) = 0xCC0099;
+				utils_draw_point(data, j, i, color.x);
 			else
-				*(int *)(data->addr + i * data->line_length + j * (data->bpp / 8)) = 0xCC000000;
+				utils_draw_point(data, j, i, color.y);
 			j++;
 		}
 		i++;
