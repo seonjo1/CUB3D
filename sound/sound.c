a99765b6b1311ad7909e4503d0612657b856b843
michang <minseok128128@gmail.com>
2024-02-25 16:13:04 +0900

    Added sound : sample code 가지고 오고 수정 완료

---
+++ sound/sound.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sound.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: michang <michang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 16:07:50 by michang           #+#    #+#             */
+/*   Updated: 2024/02/25 16:07:51 by michang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sound.h"
+
+void	sound_clear(t_sounds *sounds)
+{
+	if (sounds->attack)
+		BASS_StreamFree(sounds->attack);
+	if (sounds->flash[0])
+		BASS_StreamFree(sounds->flash[0]);
+	if (sounds->flash[1])
+		BASS_StreamFree(sounds->flash[0]);
+	if (sounds->flash[2])
+		BASS_StreamFree(sounds->flash[2]);
+	if (sounds->gun)
+		BASS_StreamFree(sounds->gun);
+	if (sounds->recall)
+		BASS_StreamFree(sounds->recall);
+	if (sounds->reload)
+		BASS_StreamFree(sounds->reload);
+	BASS_Free();
+}
+
+char	sound_load(uint32_t *s, char *path)
+{
+	static char	loaded;
+
+	loaded |= BASS_Init(-1, 48000, 0, 0, 0);
+	if (!loaded || *s)
+		return (FALSE);
+	*s = BASS_StreamCreateFile(FALSE, path, 0, 0, 0);
+	return (TRUE);
+}
+
+void	sound_play_alt(uint32_t s, char play, char loop)
+{
+	if (!s)
+		return ;
+	if (loop)
+		BASS_ChannelFlags(s,
+			BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
+	if (play)
+		BASS_ChannelPlay(s, 0);
+	else
+		BASS_ChannelPause(s);
+}
+
+void	sound_play(uint32_t s)
+{
+	if (!s)
+		return ;
+	BASS_ChannelStop(s);
+	BASS_ChannelSetPosition(s, 0, 0);
+	BASS_ChannelPlay(s, 0);
+}
+
