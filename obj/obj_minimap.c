8656f872699a4ee98c854f27e8bf4cd8112e0518
michang <minseok128128@gmail.com>
2024-02-25 14:34:47 +0900

    Update obj_minimap : 돌아가는 원 없애고 미니맵 3D 비슷하게 완성

--- obj/obj_minimap.c
+++ obj/obj_minimap.c
@@ -67,66 +67,6 @@ void	obj_draw_mini_player(t_data *data, int h)
 	}
 }
 
-char	(*obj_n_arr(void))[16]
-{
-	static char	array[16][16] = {
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
-	{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
-	};
-
-	return (array);
-}
-
-void	obj_draw_n(t_data *data, int x, int y)
-{
-	char	(*array_ptr)[16];
-	int		j;
-	int		i;
-	double	distance;
-
-	i = 0;
-	while (i < 31)
-	{
-		j = 0;
-		while (j < 31)
-		{
-			distance = sqrt((j - 15) * (j - 15) + (i - 15) * (i - 15));	
-			if (distance < 15)
-				utils_draw_point(data, x + j - 15 + data->mini.pos.x, y + i - 11 + data->mini.pos.y, 0xAAAAAA);
-			j++;
-		}
-		i++;
-	}
-	array_ptr = obj_n_arr();
-	i = 0;
-	while (i < 16)
-	{
-		j = 0;
-		while (j < 16)
-		{
-			if (array_ptr[i][j] && distance <= 20)
-				utils_draw_point(data, x + j - 8 + data->mini.pos.x, y + i - 4 + data->mini.pos.y, 0xCC0099);
-			j++;
-		}
-		i++;
-	}
-}
-
-
 void	obj_draw_minimap(t_data *data, t_mini *mini)
 {
 	int			i;
@@ -163,13 +103,11 @@ void	obj_draw_minimap(t_data *data, t_mini *mini)
 						color = 0xCC0099;
 				}
 				utils_draw_point(data, j + mini->pos.x, i + mini->pos.y, color);
-				// utils_draw_point(data, j, i, color);
 			}
 			j++;
 		}
 		i++;
 	}
 	obj_draw_mini_player(data, 5);
-	obj_draw_n(data, -(mini->size >> 1) * data->player.dir.x + (mini->size >> 1), (mini->size >> 1) * data->player.dir.y + (mini->size >> 1));
 }
 
