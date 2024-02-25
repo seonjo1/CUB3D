6f245287c3a746a71af3983d415eaa2f2d2749aa
michang <minseok128128@gmail.com>
2024-02-25 17:00:39 +0900

    Update play_action : flash 소리 구현 성공!

--- play/play_action.c
+++ play/play_action.c
@@ -95,8 +95,7 @@ void	play_action_flash(t_player *player, char *transition, char enter)
 			flash_dir = vec2_creat(1, 0);
 		player->flash_frame = 0;
 		flash_dir = vec2_normalize(flash_dir, 0.0085 * 1450);
-		printf("here: %u\n",player->s_res->attack);
-		// sound_play(player->s_res->flash[0]);
+		sound_play(player->s_res->flash[0]);
 	}
 	else if (enter == RUN)
 	{
