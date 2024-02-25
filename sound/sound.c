95956abf483de3f9c70740f768cd1b9f9838b56d
michang <minseok128128@gmail.com>
2024-02-25 17:27:15 +0900

    Update sound : sample로 소리 출력하는 것으로 바꿈

--- sound/sound.c
+++ sound/sound.c
@@ -20,7 +20,7 @@ t_sounds	*sound_init(t_sounds *s_res)
 	done &= sound_load(&(s_res->attack), "./res/sound/attack.wav");
 	done &= sound_load(&(s_res->flash[0]), "./res/sound/flash_0.wav");
 	done &= sound_load(&(s_res->flash[1]), "./res/sound/flash_1.wav");
-	done &= sound_load(&(s_res->flash[2]), "./res/sound/flash_0.wav");
+	done &= sound_load(&(s_res->flash[2]), "./res/sound/flash_2.wav");
 	done &= sound_load(&(s_res->gun), "./res/sound/gun.wav");
 	done &= sound_load(&(s_res->recall), "./res/sound/recall.wav");
 	done &= sound_load(&(s_res->reload), "./res/sound/reload.wav");
@@ -36,7 +36,7 @@ void	sound_clear(t_sounds *s_res)
 	if (s_res->flash[0])
 		BASS_StreamFree(s_res->flash[0]);
 	if (s_res->flash[1])
-		BASS_StreamFree(s_res->flash[0]);
+		BASS_StreamFree(s_res->flash[1]);
 	if (s_res->flash[2])
 		BASS_StreamFree(s_res->flash[2]);
 	if (s_res->gun)
@@ -48,34 +48,31 @@ void	sound_clear(t_sounds *s_res)
 	BASS_Free();
 }
 
-char	sound_load(unsigned int *sound, char *path)
-{
-	static char	loaded;
+char sound_load(unsigned int *sample, char *path) {
+    static char loaded;
 
-	loaded |= BASS_Init(-1, 48000, 0, 0, 0);
-	if (!loaded || *sound)
-		return (FALSE);
-	*sound = BASS_StreamCreateFile(FALSE, path, 0, 0, 0);
-	return (TRUE);
+    loaded |= BASS_Init(-1, 48000, 0, 0, 0);
+    if (!loaded || *sample)
+        return FALSE;
+    *sample = BASS_SampleLoad(FALSE, path, 0, 0, 3, 0);
+    return (*sample != 0);
 }
 
-void	sound_play_alt(unsigned int sound, char play, char loop)
-{
-	if (!sound)
-		return ;
-	if (loop)
-		BASS_ChannelFlags(sound, BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
-	if (play)
-		BASS_ChannelPlay(sound, 0);
-	else
-		BASS_ChannelPause(sound);
-}
+// void	sound_play_alt(unsigned int sound, char play, char loop)
+// {
+// 	if (!sound)
+// 		return ;
+// 	if (loop)
+// 		BASS_ChannelFlags(sound, BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
+// 	if (play)
+// 		BASS_ChannelPlay(sound, 0);
+// 	else
+// 		BASS_ChannelPause(sound);
+// }
 
-void	sound_play(unsigned int sound)
-{
-	if (!sound)
-		return ;
-	BASS_ChannelStop(sound);
-	BASS_ChannelSetPosition(sound, 0, 0);
-	BASS_ChannelPlay(sound, 0);
+void sound_play(unsigned int sample) {
+	unsigned int channel;
+	
+	channel = BASS_SampleGetChannel(sample, FALSE);
+	BASS_ChannelPlay(channel, FALSE);
 }
