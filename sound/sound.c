dfbb3cf1631b7631f31d42af7cf469de0dfb56ad
michang <minseok128128@gmail.com>
2024-02-25 16:23:14 +0900

    Update sound : refactor for clean code

--- sound/sound.c
+++ sound/sound.c
@@ -12,70 +12,69 @@
 
 #include "sound.h"
 
-void	sound_init(t_sounds *sounds)
+void	sound_init(t_sounds *s_res)
 {
 	int	done;
 
 	done = 1;
-	done &= sound_load(sounds->attack, "./res/sound/attack.wav");
-	done &= sound_load(sounds->flash[0], "./res/sound/flash_0.wav");
-	done &= sound_load(sounds->flash[1], "./res/sound/flash_1.wav");
-	done &= sound_load(sounds->flash[2], "./res/sound/flash_0.wav");
-	done &= sound_load(sounds->gun, "./res/sound/gun.wav");
-	done &= sound_load(sounds->recall, "./res/sound/recall.wav");
-	done &= sound_load(sounds->reload, "./res/sound/reload.wav");
+	done &= sound_load(s_res->attack, "./res/sound/attack.wav");
+	done &= sound_load(s_res->flash[0], "./res/sound/flash_0.wav");
+	done &= sound_load(s_res->flash[1], "./res/sound/flash_1.wav");
+	done &= sound_load(s_res->flash[2], "./res/sound/flash_0.wav");
+	done &= sound_load(s_res->gun, "./res/sound/gun.wav");
+	done &= sound_load(s_res->recall, "./res/sound/recall.wav");
+	done &= sound_load(s_res->reload, "./res/sound/reload.wav");
 	if (!done)
 		exit(1);
 }
 
-void	sound_clear(t_sounds *sounds)
+void	sound_clear(t_sounds *s_res)
 {
-	if (sounds->attack)
-		BASS_StreamFree(sounds->attack);
-	if (sounds->flash[0])
-		BASS_StreamFree(sounds->flash[0]);
-	if (sounds->flash[1])
-		BASS_StreamFree(sounds->flash[0]);
-	if (sounds->flash[2])
-		BASS_StreamFree(sounds->flash[2]);
-	if (sounds->gun)
-		BASS_StreamFree(sounds->gun);
-	if (sounds->recall)
-		BASS_StreamFree(sounds->recall);
-	if (sounds->reload)
-		BASS_StreamFree(sounds->reload);
+	if (s_res->attack)
+		BASS_StreamFree(s_res->attack);
+	if (s_res->flash[0])
+		BASS_StreamFree(s_res->flash[0]);
+	if (s_res->flash[1])
+		BASS_StreamFree(s_res->flash[0]);
+	if (s_res->flash[2])
+		BASS_StreamFree(s_res->flash[2]);
+	if (s_res->gun)
+		BASS_StreamFree(s_res->gun);
+	if (s_res->recall)
+		BASS_StreamFree(s_res->recall);
+	if (s_res->reload)
+		BASS_StreamFree(s_res->reload);
 	BASS_Free();
 }
 
-char	sound_load(uint32_t *s, char *path)
+char	sound_load(uint32_t *sound, char *path)
 {
 	static char	loaded;
 
 	loaded |= BASS_Init(-1, 48000, 0, 0, 0);
-	if (!loaded || *s)
+	if (!loaded || *sound)
 		return (FALSE);
-	*s = BASS_StreamCreateFile(FALSE, path, 0, 0, 0);
+	*sound = BASS_StreamCreateFile(FALSE, path, 0, 0, 0);
 	return (TRUE);
 }
 
-void	sound_play_alt(uint32_t s, char play, char loop)
+void	sound_play_alt(uint32_t sound, char play, char loop)
 {
-	if (!s)
+	if (!sound)
 		return ;
 	if (loop)
-		BASS_ChannelFlags(s,
-			BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
+		BASS_ChannelFlags(sound, BASS_SAMPLE_LOOP, BASS_SAMPLE_LOOP);
 	if (play)
-		BASS_ChannelPlay(s, 0);
+		BASS_ChannelPlay(sound, 0);
 	else
-		BASS_ChannelPause(s);
+		BASS_ChannelPause(sound);
 }
 
-void	sound_play(uint32_t s)
+void	sound_play(uint32_t sound)
 {
-	if (!s)
+	if (!sound)
 		return ;
-	BASS_ChannelStop(s);
-	BASS_ChannelSetPosition(s, 0, 0);
-	BASS_ChannelPlay(s, 0);
+	BASS_ChannelStop(sound);
+	BASS_ChannelSetPosition(sound, 0, 0);
+	BASS_ChannelPlay(sound, 0);
 }
