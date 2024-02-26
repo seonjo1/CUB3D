74a8e87fa72db62f5e262b7a8e0912054b3d3aea
michang <minseok128128@gmail.com>
2024-02-26 15:37:55 +0900

    Update hand : 총 소리 구현 성공

--- hand/hand.c
+++ hand/hand.c
@@ -14,6 +14,17 @@
 #include "../sound/sound.h"
 #include "../libft_s/libft_s.h"
 
+int	hand_stop_shot(char set, unsigned int c)
+{
+	static unsigned int	channel = 0;
+
+	if (set == TRUE)
+		channel = c;
+	else if (channel)
+		sound_stop(channel);
+	return (0);
+}
+
 int	hand_reset_reload(int *kb, int *rf, char set, unsigned int c)
 {
 	static unsigned int	channel = 0;
@@ -73,6 +84,7 @@ void	*hand_action_walk(t_player *player, void **arr, int delay)
 	static int	cnt = 0;
 	static int	t = 0;
 
+	hand_stop_shot(FALSE, 0);
 	if (!(player->move.x || player->move.y))
 		return (arr[t]);
 	if (++cnt > delay)
@@ -89,12 +101,13 @@ void	*hand_action_recall(void **arr, t_player *player, int *magazine)
 	int	t;
 
 	hand_reset_reload(&(player->keybinds), &(player->reload_frame), FALSE, 0);
+	hand_stop_shot(FALSE, 0);
 	if (player->recall.frame == 2)
 		sound_play(player->s_res->recall);
 	if (player->recall.frame >= HN_RECALL << 1)
 	{
 		t = HN_RECALL - 1;
-		*magazine = 50;
+		*magazine = MAX_MAG;
 	}
 	else
 		t = (player->recall.frame >> 1);
@@ -105,32 +118,38 @@ void	*hand_action_flash(void **arr, t_player *player)
 {
 	int	t;
 
+	hand_stop_shot(FALSE, 0);
 	t = player->flash_frame - 1;
 	if (player->flash_frame >= HN_FLASH)
 		t = HN_FLASH - 1;
 	return (arr[t]);
 }
 
-void	*hand_action_shot(void **arr, int *kb, int *magazine)
+void	*hand_action_shot(void **arr, t_data *data, int *magazine)
 {
-	static int	t = 0;
+	static long long	time = 0;
+	static int			t = 0;
 
-	if (*magazine >= 0)
+	if (data->time - 1 != time)
+		hand_stop_shot(TRUE, sound_play(data->s_res.gun));
+	if (*magazine > 0)
 	{
+		time = data->time;
 		(*magazine)--;
 		if (++t == ((HN_SHOT - 1) << 1))
 			t = 0;
 		return (arr[t >> 1]);
 	}
-	*kb |= (1 << KB_RELOAD);
+	data->player.keybinds |= (1 << KB_RELOAD);
 	return (arr[HN_SHOT - 1]);
 }
 
-void	*hand_action_attack(void **arr,  t_player *player)
+void	*hand_action_attack(void **arr, t_player *player)
 {
 	static int	t = 0;
 
 	hand_reset_reload(&(player->keybinds), &(player->reload_frame), FALSE, 0);
+	hand_stop_shot(FALSE, 0);
 	if (t == 0)
 		sound_play(player->s_res->attack);
 	if (++t == (HN_ATTACK << 1))
@@ -144,13 +163,14 @@ void	*hand_action_attack(void **arr,  t_player *player)
 
 void	*hand_action_reload(void **arr, t_player *player, int *magazine)
 {
+	hand_stop_shot(FALSE, 0);
 	if (player->reload_frame == 7)
 		hand_reset_reload(0, 0, TRUE, sound_play(player->s_res->reload));
 	player->reload_frame++;
 	if (player->reload_frame >= (HN_RELOAD << 1))
 	{
 		player->reload_frame = 0;
-		*magazine = 50;
+		*magazine = MAX_MAG;
 		player->keybinds = player->keybinds & ~(1 << KB_RELOAD);
 	}
 	return (arr[player->reload_frame >> 1]);
@@ -158,9 +178,9 @@ void	*hand_action_reload(void **arr, t_player *player, int *magazine)
 
 void	*hand_update(t_data *data)
 {
-	static void	*hand;
+	static void	*hand = 0;
+	static int	magazine = MAX_MAG;
 	char		*ps;
-	static int	magazine = 50;
 	
 	ps = data->player.state;
 	if (ps[2] == 'R')
@@ -172,7 +192,7 @@ void	*hand_update(t_data *data)
 	else if (ps[2] == 'F')
 		hand = hand_action_flash(data->h_res.flash, &(data->player));
 	else if (data->player.keybinds & (1 << KB_M_LEFT))
-		hand = hand_action_shot(data->h_res.shot, &(data->player.keybinds), &magazine);
+		hand = hand_action_shot(data->h_res.shot, data, &magazine);
 	else if (!ft_strncmp(ps, "W__", 4))
 		hand = hand_action_walk(&(data->player), data->h_res.walk, 3);
 	else if (!ft_strncmp(ps, "R__", 4))
