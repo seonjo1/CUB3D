0c15b717677ad1449223f4c759d4dcbe7246ca32
michang <minseok128128@gmail.com>
2024-02-26 21:57:52 +0900

    Update rc_draw_floor : 최적화를 위해 비트연산 적극 차용

--- rc/rc_draw_floor.c
+++ rc/rc_draw_floor.c
@@ -16,18 +16,18 @@ void	rc_floor_draw_pixel(t_data *data, int x, int y)
 {
 	t_rc_floor f_data;
 
-	f_data.camera_x = 2 * x / (double) WIN_WIDTH - 1;
+	f_data.camera_x = (x << 1) / (double) WIN_WIDTH - 1;
 	f_data.camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
 	f_data.ray.x = data->player.dir.x + data->player.plane.x * f_data.camera_x;
 	f_data.ray.y = data->player.dir.y + data->player.plane.y * f_data.camera_x;
-	f_data.floor_x = data->player.pos.x + f_data.ray.x * (f_data.camera_y / 2) / y;
-	f_data.floor_y = data->player.pos.y + f_data.ray.y * (f_data.camera_y / 2) / y;
+	f_data.floor_x = data->player.pos.x + f_data.ray.x * (f_data.camera_y >> 1) / y;
+	f_data.floor_y = data->player.pos.y + f_data.ray.y * (f_data.camera_y >> 1) / y;
 	if (f_data.floor_x >= 0 && f_data.floor_y >= 0 && f_data.floor_x < data->map.col && f_data.floor_y < data->map.row)
 	{
 		f_data.tex_x = (int)((ceil(f_data.floor_x) - f_data.floor_x) * data->tex[TC_F].width);
 		f_data.tex_y = (int)((ceil(f_data.floor_y) - f_data.floor_y) * data->tex[TC_F].height);
 		f_data.color = data->tex[TC_F].data[f_data.tex_y][f_data.tex_x];
-		utils_draw_point(data, x, y + WIN_HEIGHT / 2, f_data.color);
+		utils_draw_point(data, x, y + (WIN_HEIGHT >> 1), f_data.color);
 	}
 }
 
