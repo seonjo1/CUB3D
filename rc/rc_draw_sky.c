30f8090028f9243b7e738fd36be8959593c29ca4
seonjo1 <tjdgus9684@gmail.com>
2024-02-26 20:00:26 +0900

    Update rc_draw_sky : yaw가 0 ~ 6.28안에 있게 조정

--- rc/rc_draw_sky.c
+++ rc/rc_draw_sky.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:24:10 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/26 16:54:56 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/26 20:00:04 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,30 @@
 
 void	rc_draw_sky(t_data *data)
 {
+	double	yaw;
+	double	mod;
 	double	start;
 	double	tex_len;
 	
+	yaw = data->player.euler_dir.y;
+	if (yaw < 0)
+	{
+		mod = fmod(yaw, 6.28);
+		yaw = 6.28 + mod;
+	}
+	else if (yaw >= 6.28)
+		yaw = fmod(yaw, 6.28);
+	// printf("yaw : %f\n", yaw);
 	tex_len = data->sky.width * (FOV_BASE / (M_PI * 2));
-	printf("yaw:%f\n", data->player.euler_dir.y);
-	start = (data->player.euler_dir.y * data->sky.width) / (M_PI * 2) - tex_len / 2;
+	start = (yaw * data->sky.width) / (M_PI * 2) - tex_len / 2;
 	if (start < 0)
 		start += data->sky.width;
+	// printf("start : %f sky_width : %d\n", start, data->sky.width);
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, -start, -WIN_HEIGHT / 2);
-	// if (start + tex_len >= data->sky.width)
-	// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, data->sky.width - start, - WIN_HEIGHT / 2);
+	if (data->sky.width - start < WIN_WIDTH)
+	{
+		// printf("sky_width - start : %f\n", data->sky.width - start);
+		// printf("sky_width - start : %d\n",(int) (data->sky.width - start));
+		mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, (int)(data->sky.width - start), - WIN_HEIGHT / 2);
+	}
 }
