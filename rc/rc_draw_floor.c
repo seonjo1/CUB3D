01a4d4f436d687736fdbd676897882b56187ba7b
michang <minseok128128@gmail.com>
2024-02-26 21:59:13 +0900

    Update rc_draw_floor : 최적화를 위한 반복 계산 최소화

--- rc/rc_draw_floor.c
+++ rc/rc_draw_floor.c
@@ -11,77 +11,86 @@
 /* ************************************************************************** */
 
 #include "rc.h"
+#include "../libft_s/libft_s.h"
 
-void	rc_floor_draw_pixel(t_data *data, int x, int y)
+void	rc_floor_draw_pixel(t_data *data, t_intvec2 xy, double cx, int cy)
 {
 	t_rc_floor f_data;
 
-	f_data.camera_x = (x << 1) / (double) WIN_WIDTH - 1;
-	f_data.camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
-	f_data.ray.x = data->player.dir.x + data->player.plane.x * f_data.camera_x;
-	f_data.ray.y = data->player.dir.y + data->player.plane.y * f_data.camera_x;
-	f_data.floor_x = data->player.pos.x + f_data.ray.x * (f_data.camera_y >> 1) / y;
-	f_data.floor_y = data->player.pos.y + f_data.ray.y * (f_data.camera_y >> 1) / y;
-	if (f_data.floor_x >= 0 && f_data.floor_y >= 0 && f_data.floor_x < data->map.col && f_data.floor_y < data->map.row)
+	f_data.ray.x = data->player.dir.x + data->player.plane.x * cx;
+	f_data.ray.y = data->player.dir.y + data->player.plane.y * cx;
+	f_data.floor_x = data->player.pos.x + f_data.ray.x * (cy >> 1) / xy.y;
+	f_data.floor_y = data->player.pos.y + f_data.ray.y * (cy >> 1) / xy.y;
+	if (f_data.floor_x >= 0 && f_data.floor_y >= 0
+		&& f_data.floor_x < data->map.col && f_data.floor_y < data->map.row)
 	{
 		f_data.tex_x = (int)((ceil(f_data.floor_x) - f_data.floor_x) * data->tex[TC_F].width);
 		f_data.tex_y = (int)((ceil(f_data.floor_y) - f_data.floor_y) * data->tex[TC_F].height);
 		f_data.color = data->tex[TC_F].data[f_data.tex_y][f_data.tex_x];
-		utils_draw_point(data, x, y + (WIN_HEIGHT >> 1), f_data.color);
+		utils_draw_point(data, xy.x, xy.y + (WIN_HEIGHT >> 1), f_data.color);
 	}
 }
 
 void	rc_floor_thread0(t_data *data)
 {
-	int		x;
-	int		y;
+	t_intvec2	xy;
+	double		camera_x;
+	int			camera_y;
 
-	y = 1;
-	while (y < WIN_HEIGHT / 6)
+	camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
+	xy.y = 1;
+	while (xy.y < WIN_HEIGHT / 6)
 	{
-		x = 0;
-		while (x < WIN_WIDTH)
+		xy.x = 0;
+		while (xy.x < WIN_WIDTH)
 		{
-			rc_floor_draw_pixel(data, x, y);
-			x++;
+			camera_x = (xy.x << 1) / (double) WIN_WIDTH - 1;
+			rc_floor_draw_pixel(data, xy, camera_x, camera_y);
+			xy.x++;
 		}
-		y++;
+		xy.y++;
 	}
 }
 
 void	rc_floor_thread1(t_data *data)
 {
-	int		x;
-	int		y;
+	t_intvec2	xy;
+	double		camera_x;
+	int			camera_y;
 
-	y = WIN_HEIGHT / 6;
-	while (y < WIN_HEIGHT / 3)
+	camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
+	xy.y =  WIN_HEIGHT / 6;
+	while (xy.y < WIN_HEIGHT / 3)
 	{
-		x = 0;
-		while (x < WIN_WIDTH)
+		xy.x = 0;
+		while (xy.x < WIN_WIDTH)
 		{
-			rc_floor_draw_pixel(data, x, y);
-			x++;
+			camera_x = (xy.x << 1) / (double) WIN_WIDTH - 1;
+			rc_floor_draw_pixel(data, xy, camera_x, camera_y);
+			xy.x++;
 		}
-		y++;
+		xy.y++;
 	}
 }
 
 void	rc_floor_thread2(t_data *data)
 {
-	int		x;
-	int		y;
+	t_intvec2	xy;
+	double		camera_x;
+	int			camera_y;
 
-	y = WIN_HEIGHT / 3;
-	while (y < WIN_HEIGHT / 2)
+	camera_y = WIN_HEIGHT * FOV_BASE / data->player.fov;
+	xy.y =  WIN_HEIGHT / 3;
+	while (xy.y < WIN_HEIGHT / 2)
 	{
-		x = 0;
-		while (x < WIN_WIDTH)
+		xy.x = 0;
+		while (xy.x < WIN_WIDTH)
 		{
-			rc_floor_draw_pixel(data, x, y);
-			x++;
+			camera_x = (xy.x << 1) / (double) WIN_WIDTH - 1;
+			rc_floor_draw_pixel(data, xy, camera_x, camera_y);
+			xy.x++;
 		}
-		y++;
+		xy.y++;
 	}
 }
 
