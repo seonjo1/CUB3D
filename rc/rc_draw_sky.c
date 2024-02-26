765d78de911733eec328cb6d0925168930e1263e
seonjo1 <tjdgus9684@gmail.com>
2024-02-26 19:25:26 +0900

    Update rc_draw_sky : 하늘 img붙여서 그리기 (기능고장)

--- rc/rc_draw_sky.c
+++ rc/rc_draw_sky.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:24:10 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/19 11:32:05 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/26 16:54:56 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,15 @@
 
 void	rc_draw_sky(t_data *data)
 {
-	int		x;
-	int		y;
 	double	start;
 	double	tex_len;
-	double	camera_x;
-	t_vec2	ray;
 	
-	tex_len = data->tex[TC_C].width * (FOV_BASE / (M_PI * 2));
-	start = (data->player.euler_dir.y * data->tex[TC_C].width) / (M_PI * 2) - tex_len / 2;
+	tex_len = data->sky.width * (FOV_BASE / (M_PI * 2));
+	printf("yaw:%f\n", data->player.euler_dir.y);
+	start = (data->player.euler_dir.y * data->sky.width) / (M_PI * 2) - tex_len / 2;
 	if (start < 0)
-		start += data->tex[TC_C].width;
-	y = 0;
-	while (y < WIN_HEIGHT / 2)
-	{
-		x = 0;
-		while (x < WIN_WIDTH)
-		{
-			camera_x = 2 * x / (double) WIN_WIDTH - 1;
-			ray.x = data->player.dir.x + data->player.plane.x * camera_x;
-			ray.y = data->player.dir.y + data->player.plane.y * camera_x;
-			int tex_x = (int)fmod(((x * tex_len) / WIN_WIDTH + start), data->tex[TC_C].width);
-			int tex_y = (y * data->tex[TC_C].height) / (WIN_HEIGHT / 2);
-			int color = data->tex[TC_C].data[tex_y][tex_x];
-			utils_draw_point(data, x, y, color);
-			x++;
-		}
-		y++;
-	}
+		start += data->sky.width;
+	mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, -start, -WIN_HEIGHT / 2);
+	// if (start + tex_len >= data->sky.width)
+	// 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, data->sky.width - start, - WIN_HEIGHT / 2);
 }
