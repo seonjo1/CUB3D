5feca1825c9b5e4f31b0164dce722cadfdf9f903
seonjo1 <tjdgus9684@gmail.com>
2024-02-26 19:23:15 +0900

    Update parse_tex_file : sky 텍스처 img로 받는 함수로 변경

--- parse/parse_tex_file.c
+++ parse/parse_tex_file.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 19:47:57 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/18 17:57:45 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/26 19:22:41 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,19 @@ void	parse_copy_tex_data(t_tex *tex, char *addr)
 	}
 }
 
-void	parse_copy_sky_tex(t_tex *tex, char *addr, int width)
+void	parse_copy_sky_tex(t_sky *tex, char *addr, int width, int height)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (i < tex->height)
+	while (i < height)
 	{
 		j = 0;
 		while (j < width)
 		{
-			tex->data[i][j] = ((int *)addr)[(tex->width + tex->gap) * i + (j * tex->width) / width];
+			*((int *)(tex->addr) + width * i + j) = \
+				*((int *)addr + (tex->width + tex->gap) * (i * tex->height / height) + (tex->width * j) / width);
 			j++;
 		}
 		i++;
@@ -51,26 +52,25 @@ void	parse_copy_sky_tex(t_tex *tex, char *addr, int width)
 
 void	parse_open_sky_tex(t_data *data, int fd)
 {
-	int		i;
-	int		width;
-	t_tex	*tex;
+	t_sky	*tex;
 	t_data	tmp;
 	
-	tex = &(data->tex[TC_C]);
+	tex = &(data->sky);
 	tmp.img = mlx_xpm_file_to_image(data->mlx, tex->file, \
 		&(tex->width), &(tex->height));
 	if (!tmp.img && parse_close(fd))
 		parse_error("invalid texture file");
 	tmp.addr = mlx_get_data_addr(tmp.img, &(tmp.bpp), \
 		&(tmp.line_length), &(tmp.endian));
-	tex->data = malloc(sizeof(int *) * tex->height);
-	i = 0;
-	width = WIN_WIDTH * M_PI;
-	while (i < tex->height)
-		tex->data[i++] = malloc(sizeof(int) * width);
+	tex->img = mlx_new_image(data->mlx, WIN_WIDTH * 3.14, WIN_HEIGHT);
+	if (!(tex->img) && parse_close(fd))
+		exit(1);
+	tex->addr = mlx_get_data_addr(tex->img, &(tex->bpp), \
+		&(tex->line_length), &(tex->endian));
 	tex->gap = tmp.line_length / 4 - tex->width;
-	parse_copy_sky_tex(tex, tmp.addr, width);
-	tex->width = width;
+	parse_copy_sky_tex(tex, tmp.addr, tex->line_length / 4, WIN_HEIGHT);
+	tex->width = tex->line_length / 4;
+	tex->height = WIN_HEIGHT;
 	mlx_destroy_image(data->mlx, tmp.img);
 }
 
@@ -104,12 +104,12 @@ void	parse_open_tex_file(t_data *data, int fd)
 	parse_open_sky_tex(data, fd);
 }
 
-int	parse_tex_file(t_tex *tex, char *str, int type, int *element)
+int	parse_tex_file(char **file, char *str, int type, int *element)
 {
 	if (*element & type)
 		return (1);
 	*element |= type;
-	tex->file = ft_strdup_s(str);
-	tex->file[ft_strlen(tex->file) - 1] = '\0';
+	*file = ft_strdup_s(str);
+	(*file)[ft_strlen(*file) - 1] = '\0';
 	return (0);
 }
