503efdd769e2e240d0748ffd5780a00e85b7d026
seonjo1 <tjdgus9684@gmail.com>
2024-02-26 20:17:43 +0900

    Update rc_draw_sky : 주석 제거

--- rc/rc_draw_sky.c
+++ rc/rc_draw_sky.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:24:10 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/26 20:00:04 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/26 20:17:27 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,11 @@ void	rc_draw_sky(t_data *data)
 	}
 	else if (yaw >= 6.28)
 		yaw = fmod(yaw, 6.28);
-	// printf("yaw : %f\n", yaw);
 	tex_len = data->sky.width * (FOV_BASE / (M_PI * 2));
 	start = (yaw * data->sky.width) / (M_PI * 2) - tex_len / 2;
 	if (start < 0)
 		start += data->sky.width;
-	// printf("start : %f sky_width : %d\n", start, data->sky.width);
 	mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, -start, -WIN_HEIGHT / 2);
 	if (data->sky.width - start < WIN_WIDTH)
-	{
-		// printf("sky_width - start : %f\n", data->sky.width - start);
-		// printf("sky_width - start : %d\n",(int) (data->sky.width - start));
 		mlx_put_image_to_window(data->mlx, data->mlx_win, data->sky.img, (int)(data->sky.width - start), - WIN_HEIGHT / 2);
-	}
 }
