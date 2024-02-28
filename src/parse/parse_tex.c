ce930d9ddb52953effaf69bf1cf762c13b924cb7
seonjo1 <tjdgus9684@gmail.com>
2024-02-28 20:45:31 +0900

    Update parse_tex : 바닥, 천장 텍스처 대신 바닥, 천장 색을 check하게 수정

--- src/parse/parse_tex.c
+++ src/parse/parse_tex.c
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 20:57:08 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/28 17:19:46 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/28 20:44:17 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,10 @@ void	parse_identifier_check(t_data *data, char **arr, int fd, int *element)
 		flag = parse_tex_file(&(data->tex[2].file), arr[1], PC_SO, element);
 	else if (ft_strncmp(arr[0], "NO", 3) == 0)
 		flag = parse_tex_file(&(data->tex[3].file), arr[1], PC_NO, element);
-	else if (ft_strncmp(arr[0], "FT", 3) == 0)
-		flag = parse_tex_file(&(data->tex[4].file), arr[1], PC_FT, element);
-	else if (ft_strncmp(arr[0], "CT", 3) == 0)
-		flag = parse_tex_file(&(data->sky.file), arr[1], PC_CT, element);
-	// else if (ft_strncmp(arr[0], "F", 2) == 0)
-	// 	flag = parse_tex_color(&(data->c_color), arr[1], PC_F, element);
-	// else if (ft_strncmp(arr[0], "C", 2) == 0)
-	// 	flag = parse_tex_color(&(data->f_color), arr[1], PC_C, element);
+	else if (ft_strncmp(arr[0], "F", 2) == 0)
+		flag = parse_tex_color(&(data->c_color), arr[1], PC_F, element);
+	else if (ft_strncmp(arr[0], "C", 2) == 0)
+		flag = parse_tex_color(&(data->f_color), arr[1], PC_C, element);
 	else
 		flag = 1;
 	if ((flag || arr[2] != NULL) && parse_close(fd))
