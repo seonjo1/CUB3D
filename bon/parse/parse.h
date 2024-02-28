bb19936dfc875575d894d4a287f2ad6b856cf284
seonjo1 <tjdgus9684@gmail.com>
2024-02-28 21:44:56 +0900

    Update Project : parse.h에서 parse_atoi, parse_tex_color 함수 삭제

--- bon/parse/parse.h
+++ bon/parse/parse.h
@@ -6,7 +6,7 @@
 /*   By: seonjo <seonjo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 16:35:24 by seonjo            #+#    #+#             */
-/*   Updated: 2024/02/26 15:02:25 by seonjo           ###   ########.fr       */
+/*   Updated: 2024/02/28 21:44:01 by seonjo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ void		parse_error(char *msg);
 int			parse_close(int fd);
 void		parse_make_map(t_data *data, char *file);
 void		parse_texture(t_data *data, int fd);
-long long	parse_atoi(char *str);
 int			parse_is_valid_line(char *line, int fd);
-int			parse_tex_color(int *tex_color, char *str, int type, int *element);
 void		parse_open_tex_file(t_data *data, int fd);
 int			parse_tex_file(char **file, char *str, int type, int *element);
 
